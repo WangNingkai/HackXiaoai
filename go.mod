e100f33c768e09c0da21f54870a24b4e0bb8bb63
WangNingkai <1655586865@qq.com>
2023-08-04 17:25:23 +0800

    🎉 开启新项目

---
+++ go.mod
@@ -0,0 +1,24 @@
+module hackai
+
+go 1.20
+
+require (
+	github.com/gofiber/fiber/v2 v2.48.0
+	github.com/json-iterator/go v1.1.12
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.48.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+)
---
+++ pkg/utils/utils.go
@@ -0,0 +1,170 @@
+package util
+
+import (
+	"bytes"
+	"compress/gzip"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+	"io"
+	mathRand "math/rand"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+// Exists reports whether the named file or directory exists.
+func Exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+// CreatNestedFile 给定path创建文件，如果目录不存在就递归创建
+func CreatNestedFile(path string) (*os.File, error) {
+	basePath := filepath.Dir(path)
+	if !Exists(basePath) {
+		err := os.MkdirAll(basePath, 0700)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return os.Create(path)
+}
+
+func GetRandom(length int) string {
+	var r = mathRand.New(mathRand.NewSource(time.Now().UnixNano()))
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	randomStr := make([]byte, length)
+	for i := range randomStr {
+		randomStr[i] = charset[r.Intn(len(charset))]
+	}
+	return string(randomStr)
+}
+
+func SignNonce(sSecurity string, nonce string) (string, error) {
+	decodedSSecurity, err := base64.StdEncoding.DecodeString(sSecurity)
+	if err != nil {
+		return "", err
+	}
+
+	decodedNonce, err := base64.StdEncoding.DecodeString(nonce)
+	if err != nil {
+		return "", err
+	}
+
+	hash := sha256.New()
+	hash.Write(decodedSSecurity)
+	hash.Write(decodedNonce)
+	return base64.StdEncoding.EncodeToString(hash.Sum(nil)), nil
+}
+
+func GenNonce() string {
+	nonce := make([]byte, 12)
+	_, err := rand.Read(nonce[:8])
+	if err != nil {
+		return ""
+	}
+	binary.BigEndian.PutUint32(nonce[8:], uint32(time.Now().Unix()/60))
+	return base64.StdEncoding.EncodeToString(nonce)
+}
+
+func SignData(uri string, data any, sSecurity string) url.Values {
+	var dataStr []byte
+	if s, ok := data.(string); ok {
+		dataStr = []byte(s)
+	} else {
+		var err error
+		dataStr, err = json.Marshal(data)
+		if err != nil {
+			return nil
+		}
+	}
+
+	encodedNonce := GenNonce()
+	sNonce, err := SignNonce(sSecurity, encodedNonce)
+	if err != nil {
+		return nil
+	}
+	msg := fmt.Sprintf("%s&%s&%s&data=%s", uri, sNonce, encodedNonce, dataStr)
+	sb, _ := base64.StdEncoding.DecodeString(sNonce)
+	sign := hmac.New(sha256.New, sb)
+	sign.Write([]byte(msg))
+	signature := base64.StdEncoding.EncodeToString(sign.Sum(nil))
+	return url.Values{
+		"_nonce":    {encodedNonce},
+		"data":      {string(dataStr)},
+		"signature": {signature},
+	}
+}
+
+func TwinsSplit(str, sep string, def string) (string, string) {
+	pos := strings.Index(str, sep)
+	if pos == -1 {
+		return str, def
+	}
+	return str[0:pos], str[pos+1:]
+}
+
+// StringToValue converts a string to a value.
+func StringToValue(str string) interface{} {
+	switch str {
+	case "null", "none":
+		return nil
+	case "false":
+		return false
+	case "true":
+		return true
+	default:
+		if intValue, err := strconv.Atoi(str); err == nil {
+			return intValue
+		}
+		return str
+	}
+}
+
+func StringOrValue(str string) interface{} {
+	if IsDigit(str) {
+		return StringToValue(str)
+	}
+	return str
+}
+
+func Unzip(data []byte) ([]byte, error) {
+	reader, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	return io.ReadAll(reader)
+}
+
+func IsDigit(s string) bool {
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsJSON(str string) bool {
+	var js interface{}
+	return json.Unmarshal([]byte(str), &js) == nil
+}
